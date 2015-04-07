0ee7dfc52f7e103fd0ab6d90ba244a494d73d999
Orlando Karam <orlando.karam@gmail.com>
2015-04-07 21:12:28 +0000

    scripts

---
+++ scripts/install_ubuntu.sh
@@ -0,0 +1,30 @@
+sudo apt-get install git
+
+# install geany with terminal emulator (feel free to comment if prefer favorite editor)
+sudo apt-get install -y geany libvte-dev
+
+wget http://d3kbcqa49mib13.cloudfront.net/spark-1.3.0-bin-hadoop2.4.tgz
+
+mkdir spark
+cd spark
+tar -xzvf ../spark-1.3.0-bin-hadoop2.4.tgz
+mv spark-1.3.0-bin-hadoop2.4 spark
+git clone https://github.com/okaram/spark-pycon15
+
+# install java from David Branner
+curl -L --cookie "oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u40-b26/jdk-8u40-linux-x64.tar.gz -o jdk-8u40-linux-x64.tar.gz
+tar -xvf jdk-8u40-linux-x64.tar.gz
+
+sudo mkdir -p /usr/lib/jvm
+sudo mv ./jdk1.8.0_40* /usr/lib/jvm/
+
+sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.8.0_40/bin/java" 1
+sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.8.0_40/bin/javac" 1
+sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.8.0_40/bin/javaws" 1
+
+sudo chmod a+x /usr/bin/java 
+sudo chmod a+x /usr/bin/javac 
+sudo chmod a+x /usr/bin/javaws
+sudo chown -R root:root /usr/lib/jvm/jdk1.8.0_40
+
+rm jdk-8u40-linux-x64.tar.gz
